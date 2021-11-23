821264c57d4de41039976c5d6ff30b4a03d3ab93
Pranay Thakre <thakrepranay47@gmail.com>
2021-11-23 12:31:16 +0530

    first commit

---
+++ precache-manifest.4324cdcc1dd93d177e592d0a8edad9cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987f048120dc86afa9b8eb1b19a6faf5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "12f4191220edaf9f6287",
+    "url": "./static/css/main.fea6319b.chunk.css"
+  },
+  {
+    "revision": "c2a54361802751d729b5",
+    "url": "./static/js/2.121f3ee3.chunk.js"
+  },
+  {
+    "revision": "12f4191220edaf9f6287",
+    "url": "./static/js/main.405c34c0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
